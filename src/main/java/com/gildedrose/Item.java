5acd83d86a0712342e3a6783a91dc79491b7316b
Pipe1844 <pipevifa@gmail.com>
2026-06-13 11:36:10 -0600

    refactor(Item): agregar getters y setters

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -14,8 +14,28 @@ public class Item {
         this.quality = quality;
     }
 
-   @Override
-   public String toString() {
+    public String getName() {
+        return name;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    @Override
+    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
 }
